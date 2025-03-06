5080604f90f3bc8751583ee174f18a0751cc42a4
lmdown <lmdown@163.com>
2025-03-06 09:45:28 +0800

    feat: optimize nav ux

--- frontend/assets/index2.js
+++ frontend/assets/index2.js
@@ -1,1 +1,1 @@
-import{c as i}from"./pinia.js";import"./index.js";i();
+import{g as o}from"./xterm.js";import{r}from"./debounce.js";const s=o(r());export{s as d};
---
+++ frontend/assets/index3.js
@@ -0,0 +1,1 @@
+import{c as i}from"./pinia.js";import"./index.js";i();
